188244085b1c69fc46b7b4e9a31dc784010a0330
王浩 <446505447@qq.com>
2020-08-21 18:03:00 +0800

    项目初始配置

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="appContainer">
+        <!--  顶部区域-->
+        <mt-header fixed title="固定在顶部"></mt-header>
+
+        <!-- 中间路由区域 -->
+
+        <!-- 底部tabbar区域 -->
+        <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            selected: "tab1",
+        }
+    },
+    created(){
+
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="css" scoped>
+.appContainer{
+    padding-top: 40px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+// //导入vue-router包
+// import VueRouter from 'vue-router';
+// //手动安装VueRouter
+// Vue.use(VueRouter);
+
+//导入bootstrap样式
+// import 'bootstrap/dist/css/bootstrap.css';
+
+//导入mintui组件
+import { Header, Tabbar, TabItem } from 'mint-ui';
+Vue.component(Header.name, Header);
+Vue.component(Tabbar.name, Tabbar);
+Vue.component(TabItem.name, TabItem);
+
+//导入MUI的样式
+import './lib/mui/css/mui.min.css';
+
+//导入app组件
+import app from './App.vue';
+
+//导入自定义路由模块
+// import router from './router.js';
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(app), // render 会把 el 指定的容器中，所有的内容都清空覆盖，所以 不要 把 路由的 router-view 和 router-link 直接写到 el 所控制的元素中
+    // router  // 4. 将路由对象挂载到 vm 上
+})
