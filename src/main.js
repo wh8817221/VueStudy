2f21a5ebd338bf194ee18293be1cd21f986b8dea
王浩 <446505447@qq.com>
2020-08-21 18:53:25 +0800

    底部路由配置

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import Vue from 'vue';
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 // //导入vue-router包
 // import VueRouter from 'vue-router';
 // //手动安装VueRouter
@@ -15,15 +18,17 @@ Vue.component(TabItem.name, TabItem);
 
 //导入MUI的样式
 import './lib/mui/css/mui.min.css';
+import './lib/mui/css/icons-extra.css';
+
 
 //导入app组件
 import app from './App.vue';
 
 //导入自定义路由模块
-// import router from './router.js';
+import router from './router.js';
 
 var vm = new Vue({
     el: '#app',
     render: c => c(app), // render 会把 el 指定的容器中，所有的内容都清空覆盖，所以 不要 把 路由的 router-view 和 router-link 直接写到 el 所控制的元素中
-    // router  // 4. 将路由对象挂载到 vm 上
+    router  // 4. 将路由对象挂载到 vm 上
 })
---
+++ src/router.js
@@ -0,0 +1,10 @@
+import VueRouter from 'vue-router';
+
+var router = new VueRouter({
+    routes: [ //配置路由
+
+    ],
+    linkExactActiveClass: 'mui-active'
+});
+
+export default router;
